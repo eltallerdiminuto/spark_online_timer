b04375c62ccf7ecf8f364ed1383129d3054fb7f9
Arnauti <rafa@elpulgardelpanda.com>
2015-01-12 19:22:02 +0100

    new message

--- spark_online_timer.ino
+++ spark_online_timer.ino
@@ -10,19 +10,11 @@ void setup()
 }
 void loop()
 {
-  display.drawLine(0, 0, 127, 63,WHITE);
-  display.update();
-  delay(1000);
   display.clear();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.println("Hello, world!");
-  display.setTextColor(BLACK, WHITE); // 'inverted' text
-  display.println(3.141592);
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.print("0x"); display.println(0xDEADBEEF, HEX);
+  display.println("Online timer!!");
   display.update();
   delay(2000);
   display.clear();
